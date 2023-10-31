00921414ae01993c8e3d4f7dee41f560f3463b43
kevinsoft108 <kevinsoft108@gmail.com>
2023-10-31 22:41:23 +0900

    Initial commit

---
+++ frontend/electron.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require('electron');
+// const path = require('path');
+// const url =require('url');
+const isDev = true; // require('electron-is-dev');
+const START_URL = 'http://localhost:3000';
+
+const createWindow = () => {
+    let mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        icon: '',
+        webPreferences: {
+            nodeIntegration: false,
+        },
+    });
+    const startUrl = isDev ?
+        START_URL : `file://${__dirname}/build/index.html`;
+
+    mainWindow.loadURL(startUrl);
+    if (isDev) {
+        mainWindow.webContents.openDevTools({ mode: 'right' });
+    }
+    mainWindow.on('closed', () => (mainWindow = null));
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
---
+++ readme.md
@@ -0,0 +1,31 @@
+# Goalsetter MERN app
+
+This is the goalsetter app from the [Learn the MERN Stack](https://www.youtube.com/watch?v=-0exw-9YJBo) series on YouTube.
+
+## Usage
+
+node version 18.17.1
+
+Rename the .envexample to .env and add your MONGO_URI
+
+### Run Frontend
+
+```
+cd frontend
+pnpm install
+yarn start
+```
+
+### Run Server
+
+```
+cd backend
+pnpm install
+yarn start
+```
+
+### Build Windows App
+
+```
+yarn production
+```
