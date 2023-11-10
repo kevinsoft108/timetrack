c348c31a181701ada8e03cbab01b487a316353aa
yavis1218 <niko121879@gmail.com>
2023-11-10 13:46:47 +1000

    change setupProxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,10 +1,10 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:5000',
+      target: 'http://64.227.133.148:5000',
       changeOrigin: true,
     })
   );
