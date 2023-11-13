5567a36467aee7c9cfb18eb6cd46b3d7ea6e4161
yavis1218 <niko121879@gmail.com>
2023-11-13 21:59:39 +1000

    fix errors

--- backend/routes/employRoutes.js
+++ backend/routes/employRoutes.js
@@ -167,16 +167,16 @@ router.post("/update-employ", (req, res) => {
 });
 
 /* Api to delete User */
-router.post("/delete-employ", (req, res) => {
+router.post("/delete-employ", async (req, res) => {
   try {
     if (req.body && req.body.id) {
-      Employ.findByIdAndRemove(req.body.id, (err, deletedUser) => {
-        if (deletedUser) {
-          return res.status(200).json({ title: 'User deleted successfully', status: true, deletedUser });
-        } else {
-          return res.status(404).json({ errorMessage: 'User not found', status: false });
-        }
-      });
+      const deletedUser = await Employ.findByIdAndRemove(req.body.id);
+      if (deletedUser) {
+        await TimeTrack.deleteMany({ userid: deletedUser._id });
+        return res.status(200).json({ title: 'User deleted successfully', status: true, deletedUser });
+      } else {
+        return res.status(404).json({ errorMessage: 'User not found', status: false });
+      }
     } else {
       res.status(400).json({
         errorMessage: 'Add proper parameter first!',
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Header from './components/Header';
+import AdminHeader from './components/AdminHeader';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Timetrack from './pages/Timetrack';
 import AdminDashboard from './pages/AdminDashboard';
+import UserHeader from './components/UserHeader'
 import UserLogin from './pages/userLogin';
 import UserRegister from './pages/userRegister';
 import Dashboard from './pages/Dashboard';
@@ -15,11 +16,14 @@ import socketIO from 'socket.io-client';
 const socket = socketIO.connect('http://144.126.254.71:5000/');
 
 function App() {
+  const url = window.location.href;
+  let regex = /admin/
+  const admin = regex.test(url)
   return (
     <>
       <Router>
         <div className='container'>
-          <Header />
+          {admin ? <AdminHeader /> : <UserHeader />}
           <Routes>
             <Route path='/admin/timetrack' element={<Timetrack />} />
             <Route path='/admin/login' element={<Login />} />
@@ -27,7 +31,7 @@ function App() {
             <Route path='/admin' element={<AdminDashboard socket={socket} />} />
             <Route path='/dashboard' element={<Dashboard />} />
             <Route path='/register' element={<UserRegister />} />
-            <Route path='/' element={<UserLogin />} />q
+            <Route path='/' element={<UserLogin />} />
           </Routes>
         </div>
       </Router>
