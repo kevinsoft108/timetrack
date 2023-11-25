ec453a02f4488256c1e3618881f25a7d3d51752f
yavis1218 <niko121879@gmail.com>
2023-11-25 16:30:22 +0530

    fix adminAuthService.js

--- frontend/src/features/auth/adminAuthReducer.js
+++ frontend/src/features/auth/adminAuthReducer.js
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import authService from './adminAuthService'
 
 // Get user from localStorage
-const user = JSON.parse(localStorage.getItem('user'))
+const user = JSON.parse(localStorage.getItem('admin'))
 
 const initialState = {
   user: user ? user : null,
