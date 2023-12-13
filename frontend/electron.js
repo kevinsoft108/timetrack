fd9a7c494e6361e59f359186a008442dc164549c
kevinsoft108 <kevinsoft108@gmail.com>
2023-12-13 01:44:04 -0800

    mac os camera fixed

--- frontend/electron.js
+++ frontend/electron.js
@@ -1,31 +1,34 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, systemPreferences } = require('electron');
 // const path = require('path');
 // const url =require('url');
 const isDev = true; // require('electron-is-dev');
-const isAdmin = true;
-const START_URL = isAdmin ? 'http://144.126.254.71/admin' : 'https://144.126.254.71';
+const isAdmin = false;
+//const START_URL = isAdmin ? 'http://144.126.254.71/admin' : 'https://144.126.254.71';
+const START_URL = 'https://144.126.254.71';
 app.commandLine.appendSwitch('ignore-certificate-errors')
+const camera = systemPreferences.askForMediaAccess('camera');
 
+app.on('ready', () => {
+    const createWindow = () => {
+        let mainWindow = new BrowserWindow({
+            width: 1200,
+            height: 800,
+            icon: '',
+            webPreferences: {
+                nodeIntegration: true,
+            },
+        });
+        const startUrl = isDev ? START_URL : `file://${__dirname}/build/index.html`;
 
-const createWindow = () => {
-    let mainWindow = new BrowserWindow({
-        width: 1200,
-        height: 800,
-        icon: '',
-        webPreferences: {
-            nodeIntegration: true,
-        },
-    });
-    const startUrl = isDev ? START_URL : `file://${__dirname}/build/index.html`;
+        mainWindow.loadURL(startUrl);
+        if (isDev) {
+            // mainWindow.webContents.openDevTools({ mode: 'right' });
+        }
+        mainWindow.on('closed', () => (mainWindow = null));
+    };
 
-    mainWindow.loadURL(startUrl);
-    if (isDev) {
-        // mainWindow.webContents.openDevTools({ mode: 'right' });
-    }
-    mainWindow.on('closed', () => (mainWindow = null));
-};
-
-app.on('ready', createWindow);
+    createWindow();
+});
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
