ae3713afd292c4d73125c42092c4ed1f43cf4a3b
Kui Heracles <herlacles1206@gmail.com>
2023-12-21 02:10:34 +0800

    to http://

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: '//144.126.254.71:5000',
+      target: 'http//144.126.254.71:5000',
       // target: 'http://localhost:5000',
       changeOrigin: true,
     })
